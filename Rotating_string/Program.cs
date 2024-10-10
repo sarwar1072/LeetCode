d9002b6d1cf4c57f70e5b8f35b08efeed12150f1
sarwar <sarwar.cse.diu@gmail.com>
2024-10-10 10:05:24 +0600

    rotating string

--- Rotating_string/Program.cs
+++ Rotating_string/Program.cs
@@ -4,7 +4,47 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string str1=Console.ReadLine(); 
+
+            string str2=Console.ReadLine();
+
+            bool bl= RotateAndCompare(str1, str2);
+
+            Console.WriteLine(bl);
         }
+        public static bool RotateAndCompare(string s, string goal)
+        {
+            if (s.Length != goal.Length)
+            {
+                return false;
+            }
+            if (s == goal)
+            {
+                return true;
+            }
+            char[] chars = s.ToCharArray();
+            for (int i = 0; i < s.Length; i++)
+            {
+                RotateLeft(chars);
+              // Convert the rotated character array back to a string.
+                string rotatedString = new string(chars);
+                // Check if the rotated string matches the goal.
+                if (rotatedString == goal)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private static void RotateLeft(char[] chars)
+        {
+            char firstChar = chars[0];
+            for (int i = 0; i < chars.Length - 1; i++)
+            {
+                chars[i] = chars[i + 1];
+            }
+            chars[chars.Length - 1] = firstChar;
+        }
+
     }
 }
